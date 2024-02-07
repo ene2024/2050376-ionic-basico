d2a5f03d91a91ca5a170984653d7f2cfec921a44
Jesm31 <>
2024-02-06 19:53:01 -0600

    Practica 1

---
+++ practica1.js
@@ -0,0 +1,16 @@
+function imprimir(){
+
+    var inputV = document.getElementById("hola").value;
+    alert(inputV);
+
+    console.log(inputV);
+
+
+}
+
+function borrar(){
+
+
+    document.getElementById("hola").value = ("");
+
+}
