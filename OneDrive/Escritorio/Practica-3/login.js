6dced92fa788f988cd48bb23e3d26e56fefeb4fc
Jesm31 <158004069+Jesm31@users.noreply.github.com>
2024-02-20 19:52:25 -0600

    Update login.js

--- OneDrive/Escritorio/Practica-3/login.js
+++ OneDrive/Escritorio/Practica-3/login.js
@@ -1,13 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("btnLogin").addEventListener("click", function() {
-        var usuario = document.getElementById("usuario").value;
-        alert("Login correcto");
-        document.getElementById("mensaje").innerHTML = "Login correcto " + usuario + "";
-    });
+function agregar() {
 
-    document.getElementById("btnRegistro").addEventListener("click", function() {
-        var usuario = document.getElementById("usuario").value;
-        alert("Registro con éxito");
-        document.getElementById("mensaje").innerHTML = "Registro con éxito " + usuario + "";
-    });
-});
+    var articulo = document.getElementById("articulo").value
+    var monto = document.getElementById("monto").value
+
+    console.log(articulo)
+
+    
+
+
+    }
+
+
+
+    
