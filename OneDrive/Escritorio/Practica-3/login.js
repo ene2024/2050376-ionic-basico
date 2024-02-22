405f6c9bd4e04f849f1e5f1994db7dba1028f190
Jesm31 <158004069+Jesm31@users.noreply.github.com>
2024-02-21 20:53:28 -0600

    Practica 4

--- OneDrive/Escritorio/Practica-3/login.js
+++ OneDrive/Escritorio/Practica-3/login.js
@@ -1,15 +1,26 @@
+
+
+
+document.getElementById("total").innerText = "Total: $0.00";
+
+
 function agregar() {
-
-    var articulo = document.getElementById("articulo").value
-    var monto = document.getElementById("monto").value
-
-    console.log(articulo)
-
-    
+   
+    var articulo = document.getElementById("articulo").value;
+    var monto = parseFloat(document.getElementById("monto").value);
 
 
-    }
+    var ticketElement = document.createElement("div");
+    ticketElement.innerHTML = "Articulo:" + articulo + "...........Monto: $" + monto.toFixed(2);
 
 
+    document.getElementById("ticket").appendChild(ticketElement);
 
-    
+    var totalElement = document.getElementById("total");
+    var total = parseFloat(totalElement.innerText.split("$")[1]) || 0;
+    total += monto;
+    totalElement.innerText = "Total: $" + total.toFixed(2);
+
+    document.getElementById("articulo").value = "";
+    document.getElementById("monto").value = "";
+}
