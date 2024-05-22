ad04274fb855bb1ecfe1322113e61422a2f90f60
Jesm31 <sjared912@gmail.com>
2024-05-21 18:10:03 -0600

    Practica 13 build de app

---
+++ miApp-2050376/android/app/src/main/java/com/miApp/miApp2050376/MainActivity.java
@@ -0,0 +1,5 @@
+package com.miApp.miApp2050376;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- miApp-2050376/capacitor.config.ts
+++ miApp-2050376/capacitor.config.ts
@@ -1,11 +1,17 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.miApp.miApp2050376',
   appName: 'miApp-2050376',
-  webDir: 'www',
-  server: {
-    androidScheme: 'https'
+  webDir: 'www', // Directorio de salida correcto
+  bundledWebRuntime: false,
+  plugins: {
+    Camera: {
+      permissions: {
+        camera: 'Allow $(PRODUCT_NAME) to access your camera.',
+        photos: 'Allow $(PRODUCT_NAME) to access your photos.'
+      }
+    }
   }
 };
 
